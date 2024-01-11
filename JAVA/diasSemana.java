8e48139fb3276b777662634a52ee18618b44d81f
Xolrak <carloscodvcf@gmail.com>
2024-01-11 18:40:25 +0100

    todos los archivos subidos

---
+++ JAVA/diasSemana.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+public class diasSemana {
+    public static void main(String[] args) {
+        ArrayList<String> diasSemana = new ArrayList<String>();
+        ArrayList<Integer> numeros = new ArrayList<Integer>();
+    }
+}
---
+++ JAVA/ordenaAngel.java
@@ -0,0 +1,7 @@
+import java.util.Collection;
+
+public class ordenaAngel {
+    public static void main (String [] args) {
+        Collection
+    }
+}
