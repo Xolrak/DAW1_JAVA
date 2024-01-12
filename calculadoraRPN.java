fcddf579a57a9df8076ba718122fda72d7ba99cc
Xolrak <carloscodvcf@gmail.com>
2024-01-12 18:56:34 +0100

    ActualizaciÃ³n de repositorio

--- calculadoraRPN.java
+++ calculadoraRPN.java
@@ -5,8 +5,38 @@ import java.util.Scanner;
 import java.util.Stack;
 public class calculadoraRPN {
     public static void main(String[] args) {
-        boolean pilaVacia = true
-        Stack<Integer> pila = new Stack<>();
-
+        Scanner sc = new Scanner(System.in);
+        Stack<Double> pila = new Stack<>();
+        //Declaro los dos huecos de la calculadora
+        pila.push(sc.nextDouble());
+        pila.push(sc.nextDouble());
+        String operacion = sc.nextLine();
+        //Asignamos a dos variables el resultado de la pila
+        double operador1 = pila.pop();
+        double operador2 = pila.pop();
+        double resultado;
+        //Operaciones según la elección
+        switch (operacion) {
+            case "+":
+                resultado = operador1 + operador2; 
+                System.out.println("El resultado es: " + resultado);
+                break;
+            case "-":
+                resultado = operador1 - operador2; 
+                System.out.println("El resultado es: " + resultado);
+                break;
+            case "*":
+                resultado = operador1 * operador2; 
+                System.out.println("El resultado es: " + resultado);
+                break;
+            case "/":
+                resultado = operador1 / operador2; 
+                System.out.println("El resultado es: " + resultado);
+                break;
+            default:
+                break;
+        }
+        
+        sc.close();
     }    
 }
