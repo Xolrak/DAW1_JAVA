4d7542c411f1f2e014b6ff778bdf3f9428a28224
Carlos Castañeda <carloscodvcf@gmail.com>
2024-01-13 18:50:14 +0100

    ActualizaciÃ³n de repositorio

--- calculadoraRPN.java
+++ calculadoraRPN.java
@@ -7,36 +7,38 @@ public class calculadoraRPN {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         Stack<Double> pila = new Stack<>();
-        //Declaro los dos huecos de la calculadora
-        pila.push(sc.nextDouble());
-        pila.push(sc.nextDouble());
-        String operacion = sc.nextLine();
-        //Asignamos a dos variables el resultado de la pila
-        double operador1 = pila.pop();
-        double operador2 = pila.pop();
-        double resultado;
-        //Operaciones según la elección
-        switch (operacion) {
-            case "+":
-                resultado = operador1 + operador2; 
-                System.out.println("El resultado es: " + resultado);
-                break;
-            case "-":
-                resultado = operador1 - operador2; 
-                System.out.println("El resultado es: " + resultado);
-                break;
-            case "*":
-                resultado = operador1 * operador2; 
-                System.out.println("El resultado es: " + resultado);
-                break;
-            case "/":
-                resultado = operador1 / operador2; 
-                System.out.println("El resultado es: " + resultado);
-                break;
-            default:
-                break;
-        }
         
+        //Operaciones según la elección
+        while (true) {
+            //Declaro los dos huecos de la calculadora
+            pila.push(sc.nextDouble());
+            pila.push(sc.nextDouble());
+            String operacion = sc.nextLine();
+            //Asignamos a dos variables el resultado de la pila
+            double operador1 = pila.pop();
+            double operador2 = pila.pop();
+            double resultado;
+            switch (operacion) {
+                case "+":
+                    resultado = operador1 + operador2; 
+                    System.out.println("El resultado es: " + resultado);
+                    break;
+                case "-":
+                    resultado = operador1 - operador2; 
+                    System.out.println("El resultado es: " + resultado);
+                    break;
+                case "*":
+                    resultado = operador1 * operador2; 
+                    System.out.println("El resultado es: " + resultado);
+                    break;
+                case "/":
+                    resultado = operador1 / operador2; 
+                    System.out.println("El resultado es: " + resultado);
+                    break;
+                default:
+                    break;
+            }    
         sc.close();
+        }
     }    
 }
