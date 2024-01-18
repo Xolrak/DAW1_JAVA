6da54552ba617523f10c9b33eb34d31a38b9faff
Carlos Castañeda <carloscodvcf@gmail.com>
2024-01-18 12:45:17 +0100

    Actualizacion de repositorio

--- Unidad7/estadistica.java
+++ Unidad7/estadistica.java
@@ -1,6 +1,19 @@
+//Carlos C de DAW1
+import java.util.ArrayList;
 import java.util.Scanner;
 public class estadistica {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        //Hago una array en formato double para que pueda tener
+        ArrayList<Double> numeros = new ArrayList<>(); 
+        while (true) {
+            System.out.println("Introduzca un número: ");
+            String entrada = sc.nextLine();
+            //
+            if (sc.hasNextLine()) {
+                
+            }            
+        }
+        sc.close();
     }
 }
