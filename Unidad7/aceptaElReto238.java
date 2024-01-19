cafbc483eef8874f41fd7324bca8c1977ba386af
Xolrak <carloscodvcf@gmail.com>
2024-01-19 16:48:06 +0100

    Actualizacion de repositorio

---
+++ Unidad7/aceptaElReto238.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+public class aceptaElReto238 {
+    public static void main (String[] args) {
+        Scanner sc = new Scanner(System.in);
+        //Guardo el valor de los billetes
+        int[] valorBillete = {10, 20, 50, 200, 500};
+        System.out.println("Cantidad de billetes a repartir: ");
+        int billetes = sc.nextInt();
+        System.out.println("Cantidad de miembros de la banda: ");
+        int thief = sc.nextInt();
+        sc.close();
+    }
+}
