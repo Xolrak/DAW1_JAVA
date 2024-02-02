158a9e3f2f04fc4523a4622deccddbdaffbc9ccc
Xolrak <carloscodvcf@gmail.com>
2024-02-02 15:19:02 +0100

    Actualizacion de repositorio

--- Unidad8/pizzas.java
+++ Unidad8/pizzas.java
@@ -1,4 +1,12 @@
 //Carlos C de DAW1
 public class pizzas {
-    
+    //variables
+    private String tamano;
+    private String tipo;
+    private String estado;
+
+    public pizzas () {
+        this.estado = "pedida";
+        
+    }
 }
