23d59426bcd0b5bca820e680798fd150a5be33f9
Carlos Castañeda <carloscodvcf@gmail.com>
2024-02-27 19:46:09 +0100

    Actualizacion de repositorio

---
+++ Unidad10/InOK.java
@@ -0,0 +1,17 @@
+// Carlos C de DAW1 ^^
+// librerias importadas
+import java.util.Scanner;
+
+public class InOK {
+    
+    
+    
+    
+    
+    
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+    }
+}
