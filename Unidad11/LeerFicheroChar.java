958b001ab4a6e263d408219018a6b88d4fdd0d1c
Carlos Castañeda <carloscodvcf@gmail.com>
2024-03-08 16:44:56 +0100

    Actualizacion de repositorio

---
+++ Unidad11/LeerFicheroChar.java
@@ -0,0 +1,24 @@
+// Carlos C de DAW1 ^^ //
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class LeerFicheroChar {
+    public static void main(String[] args) {
+        try {
+            File f = new File(args[0]);
+            FileReader fr = new FileReader(f);
+            int letra = fr.read();
+            System.out.println();
+            while (letra != -1) {
+                System.out.print((char)letra);
+                letra = fr.read();
+            }
+            System.out.println();
+            fr.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
