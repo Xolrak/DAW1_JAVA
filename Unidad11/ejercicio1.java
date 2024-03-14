c87644612e8fcc36eb271732cb7eb1eff84557fe
Carlos Castañeda <carloscodvcf@gmail.com>
2024-03-14 02:40:16 +0100

    Actualizacion de repositorio

--- Unidad11/ejercicio1.java
+++ Unidad11/ejercicio1.java
@@ -4,16 +4,27 @@ import java.io.*;
 import java.util.*;
 public class ejercicio1 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         String texto;
+        boolean bucle = false;
         File file = new File("texto.txt");
-        try {
-            // se comprueba que el archivo exista, si no se crea
-            if (!file.exists()) {
-                file.createNewFile();
+        while (!bucle) {
+            System.out.println("Escribe lo que quieres guardar en el archivo: ");
+            texto = sc.nextLine();
+            texto = ("\n" + texto);
+            try {
+                // se comprueba que el archivo exista, si no se crea
+                if (!file.exists()) {
+                    file.createNewFile();
+                }
+                FileWriter entrada = new FileWriter("texto.txt", true);
+                entrada.write(texto);
+                // cierro el archivo
+                entrada.close();
+            } catch (IOException e) {
+                e.printStackTrace();
             }
-            FileWriter entrada = new FileWriter("texto.txt", true);
-        } catch (IOException e) {
-            e.printStackTrace();
         }
+        sc.close();
     }
 }
