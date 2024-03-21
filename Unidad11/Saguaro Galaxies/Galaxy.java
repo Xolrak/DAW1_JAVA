7774c67453ac119b0e538858bc9c1b63a6026156
Carlos Castañeda <carloscodvcf@gmail.com>
2024-03-22 00:45:47 +0100

    Actualizacion de repositorio

--- Unidad11/Saguaro Galaxies/Galaxy.java
+++ Unidad11/Saguaro Galaxies/Galaxy.java
@@ -1,3 +1,4 @@
+// Carlos C de DAW1 ^^ //
 // librerias importadas
 import java.io.*;
 
@@ -10,13 +11,30 @@ public class Galaxy implements Serializable {
     private String dec;
     
     // constructor
-    public Galaxy (String object, String con, String ar, String mag, String dec) {
+    public Galaxy (String object, String con, String ra, String mag, String dec) {
         this.object = object.replaceAll("\\s", "").replaceAll("\"", "");
         this.con = con.replaceAll("\"","");
-        this.ra = ra.replaceAll("\"","");
-        this.mag = mag.replaceAll("\"","");
-        this.dec = dec.replaceAll("\"","");
+        this.ra =ra.replaceAll("\"","");
+        this.mag=mag.replaceAll("\"","");
+        this.dec=dec.replaceAll("\"","");
     }
+    public String getObject() {
+        return object;
+    }
+    public String getCon() {
+        return con;
+    }
+    public String getRa() {
+        return ra;
+    }
+    public String getDec() {
+        return dec;
+    }
+    public String getMag() {
+        return mag;
+    }
+
+
     // metodo que agrupa en un unico String
     public String toString() {
         // se concatenan las valores y se separan por comas
