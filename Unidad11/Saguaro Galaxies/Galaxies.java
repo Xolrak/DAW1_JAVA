be1c49ce34a7529f10b3d340f6d1871fca96cd5c
Carlos Castañeda <carloscodvcf@gmail.com>
2024-03-21 04:46:54 +0100

    Actualizacion de repositorio

---
+++ Unidad11/Saguaro Galaxies/Galaxies.java
@@ -0,0 +1,23 @@
+// Carlos C de DAW1 ^^ //
+// librerias importadas
+import java.util.*;
+import java.io.*;
+
+public class Galaxies {
+    public static void main(String [] args) {
+        //Scanner sc = new Scanner(System.in);
+        // variables //
+        String object;
+        String con;
+        String ra;
+        String mag;
+        String dec;
+        String linea;
+        // variable para almacenar el nombre del .txt que contiene los datos
+        File BD_Celestes = new File("SAC_DeepSky_Ver81_QCQ.TXT");
+        // variable para almacenar el nombre del .bin que almacenará los datos
+        File BDF_Celestes = new File("SAC.bin");
+
+        //sc.close;
+    }
+}
---
+++ Unidad11/Saguaro Galaxies/Galaxy.java
@@ -0,0 +1,26 @@
+// Carlos C de DAW1 ^^ //
+// librerias importadas
+import java.io.*;
+
+public class Galaxy implements Serializable {
+    // variables privadas de la clase
+    private String object;
+    private String con;
+    private String ra;
+    private String mag;
+    private String dec;
+    
+    // constructor
+    public Galaxy (String object, String con, String ar, String mag, String dec) {
+        this.object = object.replaceAll("\\s", "").replaceAll("\"", "");
+        this.con = con.replaceAll("\"","");
+        this.ra = ra.replaceAll("\"","");
+        this.mag = mag.replaceAll("\"","");
+        this.dec = dec.replaceAll("\"","");
+    }
+    // metodo que agrupa en un unico String
+    public String toString() {
+        // se concatenan las valores y se separan por comas
+        return (object + " , " + con + " , " + ra + " , " + dec + " , " + mag);
+    }
+}
